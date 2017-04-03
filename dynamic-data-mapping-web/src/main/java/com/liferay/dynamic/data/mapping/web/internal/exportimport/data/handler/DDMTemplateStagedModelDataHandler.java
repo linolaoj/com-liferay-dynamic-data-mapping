a12498163efa5a68f856c71417bd8794ecbf35b5
Lino Alves <lino.alves@liferay.com>
2017-04-03 17:18:29 -0300

    LPS-67937 using resolved classPK

--- dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/DDMTemplateStagedModelDataHandler.java
+++ dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/DDMTemplateStagedModelDataHandler.java
@@ -423,7 +423,7 @@ public class DDMTemplateStagedModelDataHandler
 				else {
 					importedTemplate = _ddmTemplateLocalService.updateTemplate(
 						userId, existingTemplate.getTemplateId(),
-						template.getClassPK(), template.getNameMap(),
+						classPK, template.getNameMap(),
 						template.getDescriptionMap(), template.getType(),
 						template.getMode(), template.getLanguage(),
 						template.getScript(), template.isCacheable(),
